515a383946dd900451238259fca9e93e88303dac
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-10-28 20:50:31 -0300

    action 4

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,14 +22,4 @@ def test_create_contato():
     assert response.status_code == 200
 
 
-def test_create_contato_400():
-    response = client.post(
-        "/",
-        json={
-            "numerodetelefone": 51234,
-            "nome": "5string1234",
-            "sobrenome": "5string1234",
-            "email": "5string1234@"
-        },
-    )
-    assert response.status_code == 400
+
