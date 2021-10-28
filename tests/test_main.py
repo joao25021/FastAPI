12ea592567e2c2ef45bc6bb30b6450e35597030a
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-10-28 20:54:08 -0300

    action 7

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,20 +4,7 @@ from app import app
 client = TestClient(app)
 
 
-def test_main_get_list():
-    response = client.get("/contatos")
-    assert response.status_code == 200
 
-
-def test_create_contato():
-    response = client.post(
-        "/",
-        json={
-            "numerodetelefone": 651234,
-            "nome": "65string1234",
-            "sobrenome": "65string1234",
-            "email": "65string1234@"
-        },
     )
     assert response.status_code == 200
 
